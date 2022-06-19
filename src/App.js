4733f00bb956d23706653985c5fe97f0fd691325
Steve Yonkeu <yokwejuste@yahoo.com>
2022-06-19 15:48:24 +0100

    feat: making basic project structure
    Signed-off-by: Steve Yonkeu <yokwejuste@yahoo.com>

--- src/App.js
+++ src/App.js
@@ -1,3 +1,18 @@
-// My home page
+import React from 'react';
+import AppContext from "./components/AppContext/AppContext";
+
+
+function App() {
+    return (
+        <div className="App">
+            <AppContext.Provider
+                value={{}}>
+            </AppContext.Provider>
+        </div>
+
+    );
+}
+
+export default App;
 
 
