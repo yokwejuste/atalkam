c8b9cf953a689996fe83bd883472cdd329ea5377
Steve Yonkeu <yokwejuste@yahoo.com>
2022-06-20 04:58:36 +0100

    refactor: code cleanup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,94 @@
-import React from 'react';
+import React, {useState} from "react";
+import "./App.css";
 import AppContext from "./components/AppContext/AppContext";
-
+import userImage from './media/user.jpg';
 
 function App() {
+    const [isActive, setActive] = useState(false);
+
+    const toggleClass = () => {
+        setActive(!isActive);
+    };
     return (
         <div className="App">
-            <AppContext.Provider
-                value={{}}>
+            <AppContext.Provider value={{toggleClass: toggleClass}}>
+                <span
+                    className={
+                        !isActive
+                            ? "hidden"
+                            : "absolute text-white text-4xl top-5 left-4 cursor-pointer"
+                    }
+                    onClick={toggleClass}
+                >
+                    <i className="bi bi-filter-left px-2 bg-gray-900 rounded-md"></i>
+                </span>
+                <div
+                    className={
+                        !isActive
+                            ? "sidebar fixed top-0 bottom-0 lg:left-0 left-[-300px] p-2 w-[300px]" +
+                            " overflow-y-auto text-center bg-white"
+                            : "left-[-300px]"
+                    }
+                >
+                    <div className="text-gray-100 text-xl">
+                        <div className="p-2.5 items-center w-24` h-56">
+                            <img className={"rounded-full object-cover mx-auto mt-16"}
+                                 src={userImage} width={86} height={60} alt=""/>
+                        </div>
+                    </div>
+                    <div className="text-gray-900 text-[15px] mt-1">Steve Yonkeu</div>
+                    <div
+                        className={"p-2.5 mt-3 flex items-center ml-4 text-gray-400" +
+                            " rounded-md px-4 duration-300 cursor-pointer"}>
+                        <i className="bi  bi-house-door text-[20px] mr-4"/>
+                        <span className="text-[16px] ml-4 font-bold uppercase">
+                            Home
+                        </span>
+                    </div>
+                    <div
+                        className={"p-2.5 mt-3 flex items-center rounded-md px-4" +
+                            "duration-300 cursor-pointer border-4 border-white border-l-blue-400 "}>
+                        <i className="bi bi-chat-dots-fill text-[20px] text-blue-400 mr-4 ml-4"/>
+                        <span className="text-[16px] ml-4 text-blue-400 font-bold uppercase">
+                            Chat
+                        </span>
+                    </div>
+                    <div
+                        className="p-2.5 mt-3 flex items-center text-gray-400 ml-4
+                         rounded-md px-4 duration-300 cursor-pointer">
+                        <i className="bi bi-person text-[20px] mr-4"/>
+                        <span className="text-[16px] ml-4 font-bold uppercase">
+                            Contact
+                        </span>
+                    </div>
+                    <div
+                        className="p-2.5 mt-3 flex items-center rounded-md ml-4
+                         text-gray-400 px-4 duration-300 cursor-pointer">
+                        <i className="bi bi-bell text-[20px] mr-4"/>
+                        <span className="text-[16px] ml-4 font-bold uppercase">
+                            Notifications
+                        </span>
+                    </div>
+                    <div
+                        className="p-2.5 mt-3 flex items-center rounded-md ml-4
+                         text-gray-400 px-4 duration-300 cursor-pointer">
+                        <i className="bi bi-calendar-date text-[20px] mr-4"/>
+                        <span className="text-[16px] ml-4 font-bold uppercase">
+                            Calendar
+                        </span>
+                    </div>
+                    <div
+                        className="p-2.5 mt-3 flex text-gray-400  items-center ml-4
+                         rounded-md px-4 duration-300 cursor-pointer">
+                        <i className="bi bi-gear text-[20px] mr-4"/>
+                        <span className="text-[16px] ml-4 font-bold uppercase">
+                            Settings
+                        </span>
+                    </div>
+                </div>
             </AppContext.Provider>
         </div>
-
     );
 }
 
 export default App;
-
-
